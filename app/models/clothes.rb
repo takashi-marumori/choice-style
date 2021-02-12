b293f1abe5721d37bd57cf1365b962f9464b5309
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-02-12 17:58:19 +0900

    ビューを大幅変更＋未ログイン時の分岐設定

--- app/models/clothes.rb
+++ app/models/clothes.rb
@@ -2,10 +2,15 @@ class Clothes < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '帽子' },
-    { id: 3, name: '上着' },
-    { id: 4, name: 'ズボン' },
-    { id: 5, name: '靴下' },
-    { id: 6, name: '靴' }
+    { id: 3, name: 'トップス' },
+    { id: 4, name: 'アウター/ジャケット' },
+    { id: 5, name: 'スーツ' },
+    { id: 6, name: 'ワンピース/ドレス' },
+    { id: 7, name: 'ボトムス' },
+    { id: 8, name: 'スカート' },
+    { id: 9, name: 'オールインワン' },
+    { id: 10, name: 'ソックス' },
+    { id: 11, name: 'シューズ' }
   ]
 
   include ActiveHash::Associations
